4a693c8f826a082a606115242c233bb579f5f33d
Shivam <shivamvats141@gmail.com>
2024-02-07 18:36:14 +0530

    Signed-off-by: Shivam <shivamvats141@gmail.com >

---
+++ corejava/src/corejava/Perimeter_of_rectangle.java
@@ -0,0 +1,18 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class Perimeter_of_rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the value of length and breath");
+		Scanner sc=new Scanner (System.in);
+		int length=sc.nextInt();
+		int breath=sc.nextInt();
+		System.out.println();
+		
+
+	}
+
+}
---
+++ corejava/src/corejava/hello.java
@@ -0,0 +1,23 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the value of a and b");
+		Scanner sc= new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("sum="+(a+b));
+		System.out.println("diff="+(a+b));
+		System.out.println("product="+(a+b));
+		System.out.println("Qut="+(a+b));
+		System.out.println("rem="+(a+b));
+	
+		
+		
+
+	}
+
+}
